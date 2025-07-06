29f6446a4b6eaa3dbd9686613e3ade4a4d935855
Mauro <mfelipxl@gmail.com>
2025-07-06 15:15:44 -0400

    feat: add a footer.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer"
 import { Hero } from "./components/Hero"
 import { Services } from "./components/Services"
 
@@ -6,7 +7,7 @@ export default function Home() {
     <div>
       <Hero />
       <Services />
-
+      <Footer />
     </div>
   )
 }
